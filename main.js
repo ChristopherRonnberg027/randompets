34df9293954dc2cbd6ced18f89eda38412721f98
Christopher <christopher.ronnberg027@gmail.com>
2020-04-27 18:44:42 +0200

    'yo'

---
+++ main.js
@@ -0,0 +1,31 @@
+const cat_result = document.getElementById("cat_result")
+const dog_result = document.getElementById("dog_result")
+const cat_btn = document.getElementById("cat_btn")
+const dog_btn = document.getElementById("dog_btn")
+
+const caturl = 'https://aws.random.cat/meow'
+const dogurl = 'https://random.dog/woof.json'
+
+cat_btn.addEventListener("click", getRandomCat)
+dog_btn.addEventListener("click", getRandomDog)
+
+function getRandomCat() {
+    fetch(caturl)
+        .then(response => response.json())
+        .then(data => {
+            cat_result.innerHTML = `<img src="${data.file}"/>`
+        })
+}
+
+function getRandomDog() {
+    fetch(dogurl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.url.includes('.mp4')) {
+                getRandomDog()
+            } else {
+                dog_result.innerHTML = `<img src="${data.url}"/>`
+            }
+
+        })
+}
